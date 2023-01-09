a08905cc5d3aa3b4303134fc52bffbc2b862146c
Norsyazfyera <norsyazfyera@gmail.com>
2023-01-09 15:41:23 +0800

    addded 'about' component

--- nextjs-portfolio/components/Main.jsx
+++ nextjs-portfolio/components/Main.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Main = () => {
     return (
@@ -6,9 +9,24 @@ const Main = () => {
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <p className='uppercase text-sm tracking-widest text-gray-600'>Let's build something together</p>
-                    <h1 className='py-4 text-gray-600'>Hi, I'am <span className='text-[#5651e5]'>Norsyazfyera</span></h1>
-                    <h2 className='py-4 text-gray-600'>A Front-End Web Developer</h2>
-                    <p>I'am front-end web developer focused on building responsive front-end web applications while learning back-end technologies</p>
+                    <h1 className='py-4 text-gray-700'>Hi, I'am <span className='text-[#5651e5]'>Norsyazfyera</span></h1>
+                    <h2 className='py-4 text-gray-700'>A Front-End Web Developer</h2>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>I'am front-end web developer focused on building responsive front-end web applications while learning back-end technologies</p>
+
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaLinkedinIn />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaGithub />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineMail />
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <BsFillPersonLinesFill />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- nextjs-portfolio/pages/index.tsx
+++ nextjs-portfolio/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import About from '../components/About'
 import { Inter } from '@next/font/google'
 
 
@@ -19,6 +20,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
+      <About />
       
     </>
   )
