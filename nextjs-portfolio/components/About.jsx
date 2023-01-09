17716a6e53cb7f2bbf56f64d5c92cd327f19169a
Norsyazfyera <norsyazfyera@gmail.com>
2023-01-09 16:05:26 +0800

    added 'about' component

--- nextjs-portfolio/components/About.jsx
+++ nextjs-portfolio/components/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+            <div className='cols-span-2'>
+                <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
+                <h3 className='py-4'>Background</h3>
+                <p className='py-2 text-gray-600'>Lorem ipsum</p>
+
+                <p className='py-2 text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
+            </div>
+            <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+                <img className='rounded-xl' src="https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" />
+            </div>
+        </div>
+    </div>
   )
 }
 
