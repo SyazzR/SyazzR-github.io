8e94840a347d852bced639ba83e16e9fdf7975c8
Norsyazfyera <norsyazfyera@gmail.com>
2023-01-09 12:27:38 +0800

    updated menu

--- nextjs-portfolio/components/Navbar.jsx
+++ nextjs-portfolio/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai'
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
     return (
@@ -42,6 +44,50 @@ const Navbar = () => {
                             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                                 <AiOutlineClose />
                             </div>
+                        </div>
+
+                        <div className='border-b border-gray-300 my-4'>
+                            <p className='w-[85%] md:w-[90%] py-4'>Let's build something legendary together</p>
+                        </div>
+
+                        <div className='py-4 flex flex-col'>
+                            <ul className='uppercase'>
+                                <Link href="/">
+                                    <li className='py-4 text-sm'>Home</li>
+                                </Link>
+                                <Link href="/">
+                                    <li className='py-4 text-sm'>About</li>
+                                </Link>
+                                <Link href="/">
+                                    <li className='py-4 text-sm'>Skills</li>
+                                </Link>
+                                <Link href="/">
+                                    <li className='py-4 text-sm'>Projects</li>
+                                </Link>
+                                <Link href="/">
+                                    <li className='py-4 text-sm'>Contact</li>
+                                </Link>
+                            </ul>
+
+                            <div className='pt-14 '>
+                                <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+                                <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <FaLinkedinIn />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <FaGithub />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <AiOutlineMail />
+                                    </div>
+                                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                        <BsFillPersonLinesFill />
+                                    </div>
+                                    
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
