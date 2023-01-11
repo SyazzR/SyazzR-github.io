d1a63855f9468eccd517b3e67e1fe16d7a529179
Norsyazfyera <norsyazfyera@gmail.com>
2023-01-11 10:24:28 +0800

    updated project components

---
+++ nextjs-portfolio/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='w-full lg:h-screen '>
+    <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Contact
--- nextjs-portfolio/pages/index.tsx
+++ nextjs-portfolio/pages/index.tsx
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar'
 import Main from '../components/Main'
 import About from '../components/About'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 import { Inter } from '@next/font/google'
 
 
@@ -23,6 +25,8 @@ export default function Home() {
       <Main />
       <About />
       <Skills />
+      <Projects />
+      <Contact />
       
     </>
   )
